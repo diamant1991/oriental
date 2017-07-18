9e10164de511318ca4e1052cfb5cd581771bbd0b
diamant1991 <diamant1988@mail.ru>
2017-07-18 15:20:54 +0300

    add files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-$(".dropdown-btn").click(function(e){e.preventDefault();var i=$(this).next();i.is(":hidden")?($(this).addClass("active"),i.slideDown(250)):($(this).removeClass("active"),i.slideUp(250))});
+$('.dropdown-btn').click(function(e) {
+  e.preventDefault()
+  var menu = $(this).next()
+  if(menu.is(":hidden")){
+    $(this).addClass('active')
+    menu.slideDown(250)
+  }
+  else{
+    $(this).removeClass('active')
+    menu.slideUp(250)
+  }
+});
+
+$('.slct').click(function(){
+  var dropBlock = $(this).parent().find('.drop');
+
+  if( dropBlock.is(':hidden') ) {
+    dropBlock.slideDown(150);
+
+    $(this).addClass('active');
+    $('.drop').find('li').click(function(){
+      var selectResult = $(this).html();
+      $(this).parent().parent().find('input').val(selectResult);
+      $(this).parent().parent().find('.slct').removeClass('active').html(selectResult);
+
+      dropBlock.slideUp(150);
+    });
+  
+  } else {
+    $(this).removeClass('active');
+    dropBlock.slideUp(150);
+  }
+
+  return false;
+});
+
+$(document).mouseup(function (e) {
+  var container = $(".drop");
+  if (container.has(e.target).length === 0){
+      container.slideUp(150);
+      $('.slct').removeClass('active');
+  }
+});
+
+$(".scroll-btn").click(function () {
+  elementClick = $(this).attr("href")
+  destination = $(elementClick).offset().top;
+  $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+  return false;
+});
