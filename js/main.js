5b0b2bf412fbdf362f84f4a9201a33394dce61e2
diamant1991 <diamant1988@mail.ru>
2017-07-21 12:00:54 +0300

    правки и модалки

--- js/main.js
+++ js/main.js
@@ -48,3 +48,43 @@ $(".scroll-btn").click(function () {
   $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
   return false;
 });
+
+ $(".fancybox").fancybox({
+  padding : 0,
+  helpers: {
+    overlay: {
+      locked: false
+    }
+  }
+});
+
+$('.input-tel').inputmask("+7(999)-999-9999"); 
+
+$('.order-modal-btn').click(function(e) {
+  e.preventDefault();
+  $('.wrapper').addClass('wrapper--blur');
+  $('#order-modal').fadeIn(400)
+  $('body').addClass('modal-open')
+});
+
+$('.modal_close').click(function(e) {
+  e.preventDefault();
+  $('.wrapper').removeClass('wrapper--blur');
+  $('#order-modal').fadeOut(400)
+  $('body').removeClass('modal-open')
+  $('#step1').removeClass('hidden')
+  $('#step2').addClass('hidden')
+  $('.step-level__item--first').removeClass('step-level__item--green').addClass('step-level__item--active')
+  $('.step-level__item--second').removeClass('step-level__item--active').addClass('step-level__item--gray')
+  $('.step-level__text--second').removeClass('step-level__text--active').addClass('step-level__text--gray')
+});
+
+
+$('.step-btn').click(function(e) {
+  e.preventDefault()
+  $('#step1').addClass('hidden')
+  $('#step2').removeClass('hidden')
+  $('.step-level__item--first').removeClass('step-level__item--active').addClass('step-level__item--green')
+  $('.step-level__item--second').removeClass('step-level__item--gray').addClass('step-level__item--active')
+  $('.step-level__text--second').removeClass('step-level__text--gray').addClass('step-level__text--active')
+});
